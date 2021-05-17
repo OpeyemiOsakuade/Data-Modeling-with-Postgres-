1cff033f8d9445e930a7f65af31b378ea413c4de
Opeyemi Osakuade <59209191+OpeyemiOsakuade@users.noreply.github.com>
2021-05-17 07:03:31 +0100

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -8,8 +8,8 @@ time_table_drop ="DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
     songplay_id SERIAL PRIMARY KEY,
-    start_time timestamp,
-    user_id INT,
+    start_time NOT NULL timestamp,
+    user_id NOT NULL INT,
     level VARCHAR,
     song_id VARCHAR,
     artist_id VARCHAR,
@@ -66,7 +66,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users(user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT DO NOTHING;
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
